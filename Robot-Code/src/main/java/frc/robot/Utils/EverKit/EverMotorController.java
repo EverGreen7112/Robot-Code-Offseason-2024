296ed467bf556d4ab91c96b368b243a35112b052
ori sharim <ori.sharim@gmail.com>
2024-10-08 12:00:31 +0300

    created project and added utils

---
+++ Robot-Code/src/main/java/frc/robot/Utils/EverKit/EverMotorController.java
@@ -0,0 +1,69 @@
+package frc.robot.Utils.EverKit;
+
+public interface EverMotorController {
+    
+    public enum IdleMode{
+        kCoast,
+        kBrake
+    }
+
+    /**
+     * get current speed(precentage (-1 - 1))
+     */
+    public double get();
+
+  
+    /**
+     * set current speed(precentage (-1 - 1))
+     */
+    public void set(double value);   
+
+    /**
+     * Invert directions of motor;
+     */
+    public void setInverted(boolean isInverted);
+    
+    /**
+     * @return if the directions of the motors are inverted.
+     */
+    public boolean getInverted();
+    
+    /**
+     * stop motor
+     */
+    public void stop();
+
+    /**
+     * follow a given motor controller. 
+     * output = target's output
+     * @param motorController - target mottor
+     */
+    public void follow(EverMotorController motorController);
+
+    /**
+     * @return this motor controller's id.
+     */
+    public int getId();
+
+    /**
+     * Set idle mode.
+     * @param idleMode - coast or brake.
+     */
+    public void setIdleMode(IdleMode idleMode);
+
+    /**
+     * @return current temperature of motor.
+     */
+    public double getTemperature();
+
+    /**
+     * Restore factory default.
+     */
+    public void restoreFactoryDefaults();
+
+    /**
+     * @return the original instance of the motor controller
+     */
+    public Object getControllerInstance();
+    
+}
